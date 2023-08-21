bee669a4aba57893713a212b7bdf800474eaf7fd
hi25779 <859028027@qq.com>
2023-08-21 20:11:07 +1200

    add footer and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./App.module.css";
+import { Footer, Header } from "./components";
+import { Row, Col } from "antd";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <Header />
+      <div className={styles["page-content"]}>
+        <Row style={{ marginTop: 20 }}>
+          <Col span={6}>
+            <div style={{ background: "red" }}>多重菜单</div>
+          </Col>
+          <Col span={18}>
+            <div style={{ background: "blue" }}>走马灯</div>
+          </Col>
+        </Row>
+      </div>
+      <Footer />
     </div>
   );
 }
